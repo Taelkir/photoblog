9747885646192e4156a71f944e19cd4150a14100
Tom Stanley <tom.stanley37@gmail.com>
2018-09-28 16:28:31 +0100

    comments and structural directives

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -7,5 +7,12 @@ import { Component } from "@angular/core";
 })
 
 export class EntryComponent {
-
+  title: string = "My First Photo";
+  photo: string = "http://placehold.it/800x500?text=A Placeholder Image";
+  description: string = "A description of My First Photo";
+  comments: any[] = [
+    {name:"John", comment:"A comment"},
+    {name:"Snake", comment:"A comment"},
+    {name:"The Boss", comment:"A comment"},
+  ]
 }
